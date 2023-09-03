fa35000ebe87cc1369deefe38bf03135eaca82e6
frknkrc44 <krc440002@gmail.com>
2023-09-03 03:23:35 +0300

    SuperMiniDB improvements
    - Remove Base64 classes
    - Switch to fromHex/toHex methods
    - Other fixes and improvements

--- app/src/main/java/org/blinksd/sdb/YACipher.java
+++ app/src/main/java/org/blinksd/sdb/YACipher.java
@@ -2,6 +2,7 @@ package org.blinksd.sdb;
 
 public class YACipher {
     private final byte[] mKey;
+    private final char mSplitKey = 0x1B;
 
     public YACipher(byte[] key) {
         mKey = key;
@@ -19,14 +20,38 @@ public class YACipher {
         }
 
         if (!raw) {
-            return BasicBase64.encode(input);
+            return toHex(input);
         }
 
         return input;
     }
 
     public byte[] decode(byte[] input) {
-        return encode(BasicBase64.decode(input), true);
+        return encode(fromHex(input), true);
+    }
+
+    private byte[] fromHex(byte[] input) {
+        String str = new String(input);
+        String[] split = str.split(String.valueOf(mSplitKey));
+
+        byte[] output = new byte[split.length];
+        for(int i = 0; i < split.length; i++) {
+            output[i] = (byte) Integer.parseInt(split[i], 16);
+        }
+
+        return output;
+    }
+
+    private byte[] toHex(byte[] input) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (byte item : input) {
+            stringBuilder.append(Integer.toHexString(item));
+            stringBuilder.append(mSplitKey);
+        }
+
+        stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length());
+        return stringBuilder.toString().getBytes();
     }
 
     public String encodeStr(String input) {
