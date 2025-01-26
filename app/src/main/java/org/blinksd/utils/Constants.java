1e304d08f7c6acaea8d9173dfe7138881b761b2c
frknkrc44 <krc440002@gmail.com>
2025-01-26 09:10:58 +0300

    feat: Set minimum keyboard height to 10%

--- app/src/main/java/org/blinksd/utils/Constants.java
+++ app/src/main/java/org/blinksd/utils/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     static final int MAX_RADIUS = 100;
     static final int MIN_TEXT_SIZE = 6;
     static final int MAX_TEXT_SIZE = 60;
-    static final int MIN_KEYBOARD_HEIGHT = 20;
+    static final int MIN_KEYBOARD_HEIGHT = 10;
     static final int MAX_KEYBOARD_HEIGHT = 80;
     static final int MAX_OTHER_VAL = 40;
     static final int MIN_ICON_MULTI = 1;
