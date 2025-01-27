a404f675cac0686af3c6c2de53652a1b63687cd0
frknkrc44 <krc440002@gmail.com>
2025-01-27 08:48:30 +0300

    perf: Detect CI to avoid errors

--- EmojiJsonCreator/create_emoji_json.sh
+++ EmojiJsonCreator/create_emoji_json.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-if [ ! -d ".mypy_cache" ]
+if [ -n "$CI" ]
 then
-  python3 -m venv .mypy_cache
-  source .mypy_cache/bin/activate
-  python3 -m ensurepip
-  python3 -m pip install requests
-else
-  source .mypy_cache/bin/activate
+  if [ ! -d ".mypy_cache" ]
+  then
+    python3 -m venv .mypy_cache
+    source .mypy_cache/bin/activate
+    python3 -m ensurepip
+    python3 -m pip install requests
+  else
+    source .mypy_cache/bin/activate
+  fi
 fi
 
 python3 emoji_parser.py
-deactivate
+[ -n "$VIRTUAL_ENV" ] && deactivate
