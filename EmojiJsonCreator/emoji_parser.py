fbcd5ce2870e0827bdb3eb5ff5967f03d83b1c5b
frknkrc44 <krc440002@gmail.com>
2025-01-27 07:03:39 +0300

    feat: Ignore skin tones and other duplicates

--- EmojiJsonCreator/emoji_parser.py
+++ EmojiJsonCreator/emoji_parser.py
@@ -2,6 +2,14 @@ from json import dumps
 from requests import get
 
 
+def get_emoji_desc(line: str) -> str:
+    first = line[line.find('#') + 2:]
+    second = first[first.find('E'):]
+    third = second[second.find(' ') + 1:]
+    return third
+
+
+
 categories: dict[str, list[str]] = {}
 request = get(
     'https://unicode.org/Public/emoji/latest/emoji-test.txt',
@@ -10,6 +18,7 @@ request = get(
 
 if request.status_code == 200:
     recent_group = ''
+    recent_approved_line = ''
     for line in request.text.splitlines():
         if line.startswith('# group: '):
             recent_group = line[line.find(':') + 2:]
@@ -19,10 +28,17 @@ if request.status_code == 200:
         if not len(recent_group):
             continue
 
-        if ';' in line and 'qualified' in line and 'E' in line:
+        if not line.startswith('#') and ';' in line and 'qualified' in line and 'E' in line:
+            if not len(recent_approved_line) or not get_emoji_desc(line).startswith(get_emoji_desc(recent_approved_line)):
+                recent_approved_line = line
+            else:
+                continue
+
             first = line[line.find('#') + 2:]
             sec = first[:first.find('E') - 1]
             categories[recent_group].append(sec)
+else:
+    exit(1)
 
 
 with open('../app/src/main/assets/emoji_list.json', 'w') as f:
