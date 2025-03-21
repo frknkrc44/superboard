25d6fb50f08ba5cda83468367e6e5fb9f0e03eae
frknkrc44 <krc440002@gmail.com>
2025-03-21 11:26:13 +0300

    fix: Improve 35+ support

--- app/src/main/java/org/blinksd/board/InputService.java
+++ app/src/main/java/org/blinksd/board/InputService.java
@@ -654,8 +654,6 @@ public final class InputService extends InputMethodService implements
             baseHeight += bottomKeyboardBarView.getLayoutParams().height;
         }
 
-        baseHeight += insetPaddingBottom;
-
         if (SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window w = getWindow().getWindow();
             assert w != null : "Window returned null";
@@ -665,9 +663,19 @@ public final class InputService extends InputMethodService implements
                 if (navbarView != null)
                     keyboardLayoutHolder.removeView(navbarView);
 
+                if (SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+                    baseHeight += insetPaddingBottom;
+                    w.setDecorFitsSystemWindows(false);
+                }
+
                 if (SDK_INT >= Build.VERSION_CODES.P && SuperDBHelper.getBooleanOrDefault(SettingMap.SET_COLORIZE_NAVBAR_ALT) && !isColorized()) {
                     w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
                     w.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+                    if (SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+                        w.setDecorFitsSystemWindows(true);
+                    }
+
                     keyboardBackground.setLayoutParams(new RelativeLayout.LayoutParams(-1, baseHeight));
 
                     boolean monetEnabled = SuperBoardApplication.getMonetColors().isMonetEnabled();
@@ -678,9 +686,6 @@ public final class InputService extends InputMethodService implements
                     w.getDecorView().setSystemUiVisibility(ColorUtils.satisfiesTextContrast(color)
                             ? View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
                             : View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-                    if (SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
-                        w.setDecorFitsSystemWindows(true);
-                    }
                 } else if (isColorized()) {
                     // I found a bug at SDK 30 (Android R)
                     // FLAG_LAYOUT_NO_LIMITS not working
@@ -689,7 +694,10 @@ public final class InputService extends InputMethodService implements
                         w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
                     else w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
                     w.setNavigationBarColor(0);
-                    keyboardBackground.setLayoutParams(new RelativeLayout.LayoutParams(-1, baseHeight + navbarH(this)));
+                    int navbarHeight = SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM
+                            ? baseHeight
+                            : baseHeight + navbarH(this);
+                    keyboardBackground.setLayoutParams(new RelativeLayout.LayoutParams(-1, navbarHeight));
                     keyboardLayoutHolder.addView(createNavbarLayout(this, c));
                 } else {
                     w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
