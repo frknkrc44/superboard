93efa036b37eccf0508710d00df45b5f273dbf86
frknkrc44 <krc440002@gmail.com>
2025-04-29 17:56:49 +0300

    fix: Combine two if states

--- app/src/main/java/org/blinksd/utils/keys/KeyRemapper.java
+++ app/src/main/java/org/blinksd/utils/keys/KeyRemapper.java
@@ -45,15 +45,12 @@ public class KeyRemapper {
         }
 
         for (var combination : key.combinations) {
-            if (combination.bitValue() == 0 && event.hasNoModifiers()) {
-                return combination.code();
-            }
-
-            if (combination.ctrl() == event.isCtrlPressed() &&
+            if ((combination.bitValue() == 0 && event.hasNoModifiers()) ||
+                    (combination.ctrl() == event.isCtrlPressed() &&
                     combination.lAlt() == event.hasModifiers(KeyEvent.META_ALT_LEFT_ON) &&
                     combination.rAlt() == event.hasModifiers(KeyEvent.META_ALT_RIGHT_ON) &&
                     combination.capsLock() == event.isCapsLockOn() &&
-                    combination.shift() == event.isShiftPressed()) {
+                    combination.shift() == event.isShiftPressed())) {
                 return combination.code();
             }
         }
