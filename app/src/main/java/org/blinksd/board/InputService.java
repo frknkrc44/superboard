c2cf0bcf586a9f2b26de6ef1e4e871dba623646a
frknkrc44 <krc440002@gmail.com>
2026-02-03 21:26:55 +0300

    feat: Add orientation detection to set prefs

--- app/src/main/java/org/blinksd/board/InputService.java
+++ app/src/main/java/org/blinksd/board/InputService.java
@@ -51,6 +51,7 @@ import android.inputmethodservice.Keyboard;
 import android.os.Build;
 import android.view.InputDevice;
 import android.view.KeyEvent;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -106,6 +107,8 @@ public final class InputService extends InputMethodService implements
     private boolean hiddenBySelf = false;
     Intent settingsActivity = null;
     final Runnable onColorsLoadedListener = this::setPrefs;
+    private OrientationEventListener mOrientationEventListener;
+    private boolean mOrientationDetectionEnabled = true;
 
     private final View.OnClickListener emojiClick = v -> {
         final int num = Integer.parseInt(v.getTag().toString());
@@ -251,6 +254,22 @@ public final class InputService extends InputMethodService implements
 
         if (superBoardView != null)
             superBoardView.updateKeyState();
+
+        if (mOrientationEventListener == null && mOrientationDetectionEnabled) {
+            mOrientationEventListener = new OrientationEventListener(this) {
+                @Override
+                public void onOrientationChanged(int orientation) {
+                    setPrefs();
+                }
+            };
+
+            if (mOrientationEventListener.canDetectOrientation()) {
+                mOrientationEventListener.enable();
+            } else {
+                mOrientationDetectionEnabled = false;
+                mOrientationEventListener = null;
+            }
+        }
     }
 
     @Override
